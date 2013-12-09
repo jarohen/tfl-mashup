fdf06b0911b7e968307661d215030f1464ca2f37
James Henderson <james@pcr.me.uk>
2013-12-09 21:29:17 +0000

    First bash at a server side

--- src/clojure/tfl_mashup/handler.clj
+++ src/clojure/tfl_mashup/handler.clj
@@ -4,12 +4,17 @@
             [compojure.route :refer [resources]]
             [compojure.handler :refer [api]]
             [hiccup.page :refer [html5 include-css include-js]]
-            [frodo :refer [repl-connect-js]]))
+            [frodo :refer [repl-connect-js]]
+            [ring.middleware.format :refer [wrap-restful-format]]
+            [clj-http.client :as http]
+            [chord.http-kit :refer [with-channel]]
+            [clojure.tools.reader.edn :as edn]
+            [clojure.core.async :as a :refer [<! >! go go-loop]]))
 
 (defn page-frame []
   (html5
    [:head
-    [:title "tfl-mashup - CLJS Single Page Web Application"]
+    [:title "Location Tracker - CLJS Single Page Web Application"]
     (include-js "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js")
     (include-js "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js")
     (include-css "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css")
@@ -20,10 +25,53 @@
      [:div#content]
      [:script (repl-connect-js)]]]))
 
+(defn send-locations! [users]
+  (let [message (-> users
+                    vals
+                    pr-str)]
+    (doseq [conn (keys users)]
+      (a/put! conn message))))
+
+(let [users-ch (a/chan)]
+  (go-loop [users {}]
+    
+    (let [tick (a/timeout 500)
+          [v c] (a/alts! [tick users-ch])]
+      (condp = c
+        tick (do
+               (send-locations! users)
+               (recur users))
+        users-ch (let [{:keys [event conn details]} v]
+                   (recur
+                    (case event
+                      :joined (assoc users conn {})
+                      :update (assoc users conn details)
+                      :left (dissoc users conn)))))))
+  
+  (defn user-connected! [conn]
+    (a/put! users-ch {:event :joined :conn conn})
+    (go-loop []
+      (if-let [{:keys [message]} (<! conn)]
+        (let [details (edn/read-string message)]
+          (>! users-ch {:event :update
+                        :conn conn
+                        :details details})
+          (recur))
+        
+        (>! {:event :left
+             :conn conn})))))
+
+(defn user-joined! [req]
+  (with-channel req user-conn
+    (user-connected! user-conn)))
+
 (defroutes app-routes
   (GET "/" [] (response (page-frame)))
+  (GET "/locations" {:as req}
+    (user-joined! req))
   (resources "/js" {:root "js"}))
 
 (def app 
-  (-> app-routes
+  (-> #'app-routes
+      (wrap-restful-format :formats [:edn :json-kw])
       api))
