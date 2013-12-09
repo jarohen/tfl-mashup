334bc27ffacf143fff7d46e2ff053f9c32281206
Ravi Kotecha <kotecha.ravi@gmail.com>
2013-12-09 23:59:03 +0000

    map works now, geolocation too. just need to send message to server/get broadcasts now

--- src/cljs/tfl_mashup/cljs/home.cljs
+++ src/cljs/tfl_mashup/cljs/home.cljs
@@ -4,6 +4,9 @@
   (:require-macros [dommy.macros :refer [node sel1]]))
 
 
+(defn on-location-found [e]
+  (.log js/console (.-latlng e)))
+
 (def tile-url
   "http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png")
 
@@ -14,12 +17,12 @@
                  (d/replace-contents! (sel1 :#content)
                                       (node [:div
                                               [:h2 {:style {:margin-top :1em}}
-                                                   "Hello world frfom ClojureScript!"]
+                                                   "Hello world from ClojureScript!"]
                                               [:div#map {:style {:width "800px" :height "600px"}}]
                                              ]))
-                 (let [map (-> L (.map "map") (.setView (clj->js [51.505 -0.09] 13)))
-                       tile-layer (.tileLayer L tile-url {:attribution "whocares"})]
-                   (.log js/console map)
-                   (.log js/console tile-layer)
-                   (.addTo tile-layer map))
+                 (let [map (-> L (.map "map") (.setView (clj->js [51.505 -0.09]) 13))]
+                   (-> L (.tileLayer tile-url (clj->js {:maxZoom 18 :attribution "OpenStreetMap"}))
+                       (.addTo map))
+                    (.on map "locationfound" on-location-found)
+                    (.locate map (clj->js {:setView true :maxZoom 16})))
                  ))))
